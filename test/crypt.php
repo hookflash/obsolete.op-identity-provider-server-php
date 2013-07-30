b579c2c3ede012f1c89c73d4eab978b9d2f5fa00
Christoph Dorn <christoph@christophdorn.com>
2013-07-30 11:00:51 -0700

    crypto fixes

--- test/crypt.php
+++ test/crypt.php
@@ -48,9 +48,9 @@ var_dump('$key', $key);
 $iv = hash('md5', "0123456789012345");
 var_dump('$iv', $iv);
 var_dump("-- encrypt --------");
-$data = "MY-DATA";
+$data = "MY-DATA-AND-HERE-IS-MORE-DATA";
 
-$td = mcrypt_module_open('rijndael-128', '', 'nofb', '');
+$td = mcrypt_module_open('rijndael-128', '', 'cfb', '');
 if (mcrypt_generic_init($td, hex2bin($key), hex2bin($iv)) != -1) {
 	$c_t = mcrypt_generic($td, $data);
 	mcrypt_generic_deinit($td);
@@ -61,7 +61,9 @@ var_dump($encrypted);
 
 var_dump("-- decrypt --------");
 
-$td = mcrypt_module_open('rijndael-128', '', 'nofb', '');
+$encrypted = "80eb666a9fc9e263faf71e87ffc94451d7d8df7cfcf2606470351dd5ac3f70bd";
+
+$td = mcrypt_module_open('rijndael-128', '', 'cfb', '');
 if (mcrypt_generic_init($td, hex2bin($key), hex2bin($iv)) != -1) {
 	$c_t = mdecrypt_generic($td, hex2bin($encrypted));
 	mcrypt_generic_deinit($td);
@@ -78,10 +80,10 @@ var_dump(bin2hex(file_get_contents("raw.txt.enc")));
 
 var_dump("-- api --------");
 
-$secretKey = "MY-SECRET-KEY";
+$secretKey = "klksd9887w6uysjkksd89893kdnvbter";
 var_dump($secretKey);
 
-$inputData = "MY-DATA";
+$inputData = '{"service":"github","consumer_key":"264ea34924b00a5fa84e","consumer_secret":"6d21988222de0f9cc3c0257b70357a5b22bd23b8","token":"ffd648ab7b9461bbfc48405dd26e0fc12aedbb57"}';
 var_dump($inputData);
 
 $encryptedData = encrypt($inputData, $secretKey);
@@ -92,8 +94,26 @@ var_dump($outputData);
 
 var_dump("-- js --------");
 
-$encryptedData = "d927ad81199aa7dcadfdb4e47b6dc694-80eb666a9fc9e2";
-$secretKey = "01234567890123456789012345678901";
+$encryptedData = "f5641be9d451ba13be27e6300ece8137-11982815e1a25816cdcac59b9282814084b212a812253ce88e1d156af52a56ce7c2a6adff0b4cb95f6200e6f33b337ca474d92a4ae6537072e1b862cf6aecb84f53cc940040ce362de11c4ef4fdfb2689028486924abbdf74982cf77ae25ffbadf8ed9e452b61aad064498790976c48885bac482343e92946066bb7a78b8ad2f79c3083e1517ba64f7a31a855bea8cdc5ba9f47608573d8506e3de221b92f62a833af593c22c100a15fa";
+$secretKey = "klksd9887w6uysjkksd89893kdnvbter";
 
 $outputData = decrypt($encryptedData, $secretKey);
 var_dump($outputData);
+
+
+
+// NOTE: This is the implementation compatible with JS cifre.
+
+var_dump("-- phpseclib --------");
+
+include('../server/var/www/idprovider.com/app/php/libs/seclib/Crypt/AES.php');
+
+$cipher = new Crypt_AES(CRYPT_AES_MODE_CFB);
+// keys are null-padded to the closest valid size
+// longer than the longest key and it's truncated
+//$cipher->setKeyLength(128);
+$cipher->setKey(hex2bin($key));
+$cipher->setIV(hex2bin($iv));
+
+var_dump(bin2hex($cipher->encrypt($data)));
+var_dump($cipher->decrypt($cipher->encrypt($data)));
