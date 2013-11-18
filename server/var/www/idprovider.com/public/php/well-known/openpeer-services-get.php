0249b5e83ab394025b8d40ac6d20278ea7c6f7fe
Adriano Maljkovic <adriano@hookflash.com>
2013-11-18 13:38:47 +0100

    made progress: https://github.com/openpeer/op-identity-provider-server-php/issues/48

---
+++ server/var/www/idprovider.com/public/php/well-known/openpeer-services-get.php
@@ -0,0 +1,17 @@
+<?php
+
+echo '{'.
+  '"result": {'.
+    '"$domain": "' . MY_DOMAIN . '",'.
+    '"$handler": "bootstrapper",'.
+    '"$method": "services-get",'.
+
+    '"error": {'.
+      '"$id": 302,'.
+      '"#text": "Found",'.
+      '"location": "' . HF_SERVICE_DOMAIN . 'services-get"'.
+    '}'.
+  '}'.
+'}'
+
+?>
