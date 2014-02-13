04843d130cc66c530a70c382fd5924aa2ad66639
Christoph Dorn <christoph@christophdorn.com>
2014-02-12 19:10:55 -0800

    better custom config - https://github.com/openpeer/hcs-system-tests/issues/6

---
+++ server/var/www/idprovider.com/app/php/config/config-custom.php
@@ -0,0 +1,52 @@
+<?php
+
+//-- IMPORTANT --//
+
+// Here you set your application's standard protocol
+define('PROTOCOL', 'https://');
+
+// Here you set your domain
+define('MY_DOMAIN', PROTOCOL . $_SERVER['HTTP_HOST']);
+
+// Here you set your database
+define('APP_DB_NAME', 'provider_db');
+define('APP_DB_HOST', 'localhost');
+define('APP_DB_USER', 'root');
+define('APP_DB_PASS', '*************');
+
+// Here you set your Hookflash service domain
+define('DOMAIN', $_SERVER['HTTP_HOST']);
+define('HF_SERVICE_DOMAIN', PROTOCOL . 'unstable.hookflash.me/');
+
+// Here you set your OAuth keys and secrets
+define('LINKEDIN_CONSUMER_KEY', '***********');
+define('LINKEDIN_CONSUMER_SECRET', '******************');
+
+define('FACEBOOK_APP_ID', '**************');
+define('FACEBOOK_APP_SECRET', '**********************************');
+
+define('TWITTER_APP_ID', '************');
+define('TWITTER_APP_SECRET', '**********************************');
+
+// Here you set your SMTP service parameters
+require(APP . 'php/config/special/smtp_config.php');
+
+// Here you set your SMS service parameters
+require(APP . 'php/config/special/sms_config.php');
+
+// Here you set your specific cryptographically random values
+define('PROVIDER_MAGIC_VALUE', '******************************');
+
+// Here you set your domain hosting secret
+define('DOMAIN_HOSTING_SECRET', '***************');
+
+// Here you set your users' avatars uploading location
+define('UPLOAD_LOCATION', ROOT . '/public/php/service/avatars/');
+
+// Where to send JS logs
+define('HF_LOGGER_HOST', 'logger.hookflash.me');
+
+define('HF_PASSWORD1_BASEURI', '***************');
+define('HF_PASSWORD2_BASEURI', '***************');
+
+//^^ IMPORTANT ^^//
--- server/var/www/idprovider.com/app/php/config/config.php
+++ server/var/www/idprovider.com/app/php/config/config.php
@@ -46,58 +46,31 @@ if ( !defined('APP') ) {
         define('APP',  ROOT . '/app/');
 }
 
-//-- IMPORTANT --//
 
-// Here you set your application's standard protocol
-define('PROTOCOL', 'https://');
-
-// Here you set your domain
-define('MY_DOMAIN', PROTOCOL . $_SERVER['HTTP_HOST']);
-
-// Here you set your database
-define('APP_DB_NAME', 'provider_db');
-define('APP_DB_HOST', 'localhost');
-define('APP_DB_USER', 'root');
-define('APP_DB_PASS', '*************');
-
-// Here you set your Hookflash service domain
-define('DOMAIN', $_SERVER['HTTP_HOST']);
-define('HF_SERVICE_DOMAIN', PROTOCOL . 'unstable.hookflash.me/');
-
-// Here you set your OAuth keys and secrets
-define('LINKEDIN_CONSUMER_KEY', '***********');
-define('LINKEDIN_CONSUMER_SECRET', '******************');
-
-define('FACEBOOK_APP_ID', '**************');
-define('FACEBOOK_APP_SECRET', '**********************************');
-
-define('TWITTER_APP_ID', '************');
-define('TWITTER_APP_SECRET', '**********************************');
-
-// Here you set your SMTP service parameters
-require(APP . 'php/config/special/smtp_config.php');
-
-// Here you set your SMS service parameters
-require(APP . 'php/config/special/sms_config.php');
-
-// Here you set your specific cryptographically random values
-define('PROVIDER_MAGIC_VALUE', '******************************');
-
-// Here you set your domain hosting secret
-define('DOMAIN_HOSTING_SECRET', '***************');
-
-// Here you set your users' avatars uploading location
-define('UPLOAD_LOCATION', ROOT . '/public/php/service/avatars/');
-
-// Where to send JS logs
-define('HF_LOGGER_HOST', 'logger.hookflash.me');
-
-define('HF_PASSWORD1_BASEURI', '***************');
-define('HF_PASSWORD2_BASEURI', '***************');
-
-//^^ IMPORTANT ^^//
+require (dirname(__FILE__) . '/config-custom.php');
 
 
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        header('Access-Control-Allow-Headers: Keep-Alive,User-Agent,X-Requested-With,Cache-Control,Content-Type,If-Modified-Since');
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        header("HTTP/1.0 204 No Content");
+        exit(0);
+}
+
+
+function siteURL($domainName="") {
+    $protocol = ((!empty( $_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+    if ($domainName=="") {
+        $domainName = $SERVER['HTTP_HOST'];
+    }
+    return $protocol.$domainName;
+}
 
 // Log events
 function APIEventLog($sText, $iErrorCode='200', $sAPISessionID='') {
@@ -118,4 +91,3 @@ function APIEventLog($sText, $iErrorCode='200', $sAPISessionID='') {
         ));
 }
 
-?>
