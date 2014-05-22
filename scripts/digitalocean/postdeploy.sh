9e2e91c27fbea772d774cde0c6c77db4756e8ff8
Christoph Dorn <christoph@christophdorn.com>
2014-05-22 13:52:40 -0700

    provision mysql

--- scripts/digitalocean/postdeploy.sh
+++ scripts/digitalocean/postdeploy.sh
@@ -37,3 +37,75 @@ apachectl graceful
 sleep 1
 echo "Apache processes after:"
 ps axuww | grep apache
+
+
+echo "Provision MySQL Database ..."
+node --eval '
+const FS = require("fs");
+const EXEC = require("child_process").exec;
+
+function parseConfig(callback) {
+	var mysqlConfig = FS.readFileSync("/etc/mysql/debian.cnf", "utf8");
+	var config = {};
+	var activeSection = null;
+	mysqlConfig.split("\n").forEach(function(line) {
+		var m = line.match(/^\[([^\]]+)\]$/);
+		if (m) {
+			activeSection = m[1];
+			if (!config[activeSection]) config[activeSection] = {};
+		} else {
+			var m = line.match(/^(\S+)\s*=\s*(.+)$/);
+			if (m) {
+				config[activeSection][m[1]] = m[2];
+			}
+		}
+	});
+	return callback(null, config);
+}
+
+function provisionTable(config, callback) {
+
+	function createDatabase(callback) {
+		var command = "cat '$PIO_SERVICE_PATH'/sync/source/idprovider.com/schema/provider_db.schema.sql | mysql -u" + config.client.user + " -p" + config.client.password;
+		console.log("Running command: " + command);
+		return EXEC(command, function(err, stdout, stderr) {
+			if (err) return callback(err);
+			return callback(null);
+		});
+	}
+
+	function checkIfExists(callback) {
+		var command = "mysql -u" + config.client.user + " -p" + config.client.password + " -D provider_db -e \"SHOW TABLES\"";
+		console.log("Running command: " + command);
+		return EXEC(command, function(err, stdout, stderr) {
+			if (err) {
+				if (/Unknown database/.test(err.message)) {
+					return callback(null, false);
+				}
+				return callback(err);
+			}
+			return callback(null, true);
+		});
+	}
+
+	return checkIfExists(function(err, exists) {
+		if (err) return callback(err);
+		if (exists) return callback(null);
+		return createDatabase(callback);
+	});
+}
+
+function main(callback) {
+	return parseConfig(function(err, config) {
+		return provisionTable(config, callback);
+	});
+}
+
+main(function(err) {
+	if (err) {
+		console.error(err.stack);
+		process.exit(1);
+	}
+	process.exit(0);
+});
+'
