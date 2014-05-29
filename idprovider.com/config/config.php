d24eee6f7ea4fb56b5ca372ec30c1e5e0cebe066
Adriano Maljkovic <adriano@hookflash.com>
2014-05-29 20:43:17 +0200

    PHP config settled

--- idprovider.com/config/config.php
+++ idprovider.com/config/config.php
@@ -46,7 +46,6 @@ if ( !defined('ROOT') ) {
 
 require_once(ROOT . 'config/config-custom.php');
 
-
 if (isset($_SERVER['HTTP_ORIGIN'])) {
         header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
         header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
